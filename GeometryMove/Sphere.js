b8bb2a4484130ea1cf387547d0513d086a8685b5
Nicolas Robles <nr.robles@uniandes.edu.co>
2019-09-02 03:36:46 -0500

    adicion del archivo para manejar el dibujo de la esfera

--- GeometryMove/Sphere.js
+++ GeometryMove/Sphere.js
@@ -17,24 +17,25 @@ function Sphere() {
         mm: mat4.create(),
         nm: null,
     };
-    this.selColor = [0.1,0.2,0.6,1.0];
+    this.selColor = [0.3,0.2,0.6,1.0];
     this.stride = 0;
+    this.objType = "Sphere";
 
     // Initialization
     this.init = function(_this) {
-    var SPHERE_DIV = 12;
+    var sphereSegments = 12;
     var i, ai, si, ci;
     var j, aj, sj, cj;
     var p1, p2;
 
     // Vertices
     var vertices = [], indices = [];
-    for (j = 0; j <= SPHERE_DIV; j++) {
-        aj = j * Math.PI / SPHERE_DIV;
-        sj = Math.sin(aj);
-        cj = Math.cos(aj);
-        for (i = 0; i <= SPHERE_DIV; i++) {
-            ai = i * 2 * Math.PI / SPHERE_DIV;
+    for (j = 0; j <= sphereSegments; j++) {
+        aj = j * Math.PI / sphereSegments;
+        sj = 1.5*Math.sin(aj);
+        cj = 1.5*Math.cos(aj);
+        for (i = 0; i <= sphereSegments; i++) {
+            ai = i * 2 * Math.PI / sphereSegments;
             si = Math.sin(ai);
             ci = Math.cos(ai);
 
@@ -46,10 +47,10 @@ function Sphere() {
     _this.attributes.aPosition.bufferData = new Float32Array(vertices);
     //_this.attributes.aColor.bufferData = new Float32Array(vertices);
     // Indices
-    for (j = 0; j < SPHERE_DIV; j++) {
-        for (i = 0; i < SPHERE_DIV; i++) {
-            p1 = j * (SPHERE_DIV+1) + i;
-            p2 = p1 + (SPHERE_DIV+1);
+    for (j = 0; j < sphereSegments; j++) {
+        for (i = 0; i < sphereSegments; i++) {
+            p1 = j * (sphereSegments+1) + i;
+            p2 = p1 + (sphereSegments+1);
 
             indices.push(p1);
             indices.push(p2);
@@ -64,8 +65,8 @@ function Sphere() {
 
     // Selection color
     var selColor = [];
-    for (j = 0; j <= SPHERE_DIV; j++) {
-        for (i = 0; i <= SPHERE_DIV; i++) {
+    for (j = 0; j <= sphereSegments; j++) {
+        for (i = 0; i <= sphereSegments; i++) {
             selColor.push(_this.selColor[0]);
             selColor.push(_this.selColor[1]);
             selColor.push(_this.selColor[2]);
@@ -79,4 +80,8 @@ function Sphere() {
         bufferData: new Float32Array(selColor),
     };*/
     }(this);
+
+    this.draw = function(gl){
+        state.gl.drawElements(gl.TRIANGLES, this.indices.length, gl.UNSIGNED_BYTE, 0);
+    };
 };
